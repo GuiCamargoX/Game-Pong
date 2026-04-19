10b78b047d5f574d2917845c70e1ad6e239aa8ec
guicamargo <gui551@hotmail.com>
2026-04-19 12:38:30 +1000

    refactor: rename server internals and centralize gameplay constants

--- Servidor/Servidor.java
+++ Servidor/Servidor.java
@@ -1,14 +1,16 @@
-import java.net.*;
-import java.io.*;
-import java.util.*;
 import java.awt.Rectangle;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 class Servidor {
+  static final int MAX_PLAYERS = 2;
+
   private static final int DEFAULT_PORT = 5050;
   private static final String PORT_ENV = "PONG_PORT";
 
   public static void main(String[] args) {
-    ServerSocket serverSocket=null;
+    ServerSocket serverSocket = null;
     int port = resolvePort();
 
     try {
@@ -18,7 +20,7 @@ class Servidor {
       System.exit(1);
     }
 
-    for (int i=0; i<3; i++) {
+    for (int i = 0; i < MAX_PLAYERS; i++) {
       Socket clientSocket = null;
       try {
         clientSocket = serverSocket.accept();
@@ -27,12 +29,9 @@ class Servidor {
         System.exit(1);
       }
 
-      System.out.println("Accept Funcionou!");
-
-      new Servindo(clientSocket).start();
-
+      System.out.println("Client connected");
+      new ClientSession(clientSocket).start();
     }
-
 
     try {
       serverSocket.close();
@@ -56,174 +55,198 @@ class Servidor {
   }
 }
 
+class ClientSession extends Thread {
+  private static final int BOARD_WIDTH = 650;
+  private static final int BOARD_HEIGHT = 480;
+  private static final int WALL_X = 10;
+  private static final int WALL_WIDTH = BOARD_WIDTH - 20;
+  private static final int WALL_THICKNESS = 6;
+  private static final int TOP_WALL_Y = 0;
+  private static final int BOTTOM_WALL_Y = BOARD_HEIGHT - WALL_THICKNESS;
+  private static final int LEFT_PADDLE_X = 30;
+  private static final int RIGHT_PADDLE_X = 590;
+  private static final int PADDLE_COLLISION_WIDTH = 17;
+  private static final int PADDLE_HEIGHT = 77;
+  private static final int BALL_SIZE = 15;
+  private static final int PADDLE_MOVE_STEP = 10;
+  private static final int INITIAL_PADDLE_Y = 250;
+  private static final int INITIAL_BALL_SPEED = 5;
+  private static final int LOOP_RATE = 60;
+  private static final long LOOP_SLEEP_MILLIS = 1000L / LOOP_RATE;
+  private static final int BALL_RESET_X = BOARD_WIDTH / 2;
+  private static final int BALL_RESET_Y = BOARD_HEIGHT / 2;
+  private static final int OPPONENT_VIEW_BALL_RESET_X = 600 / 2;
+  private static final char PLAYER_ONE_MARKER = '1';
+  private static final char PLAYER_TWO_MARKER = '2';
 
-class Servindo extends Thread {
-  Socket clientSocket;
-  static DataOutputStream os[] = new DataOutputStream[2];
-  static int cont=0;
-  public boolean up, down;
+  private final Socket clientSocket;
+  static DataOutputStream[] clientOutputs = new DataOutputStream[Servidor.MAX_PLAYERS];
+  static int connectedClients = 0;
+  public boolean moveUp;
+  public boolean moveDown;
 
-  static int BallX=650/2 ,BallY=480/2;//screen player1
-  static int Ball2X=650/2; //screen player2
-  static int player1=250 , player2=250;
-  static int fator1=5, fator2=5;
-  static int playerOneScore = 0, playerTwoScore = 0;
-  static boolean showTitleScreen=true;
+  static int ballX = BALL_RESET_X;
+  static int mirroredBallX = BALL_RESET_X;
+  static int ballY = BALL_RESET_Y;
+  static int playerOneY = INITIAL_PADDLE_Y;
+  static int playerTwoY = INITIAL_PADDLE_Y;
+  static int ballStepX = INITIAL_BALL_SPEED;
+  static int ballStepY = INITIAL_BALL_SPEED;
+  static int playerOneScore = 0;
+  static int playerTwoScore = 0;
+  static boolean showTitleScreen = true;
 
-	Rectangle parede_cima = new Rectangle(10, 0 , 650-20 , 6);
-	Rectangle parede_baixo= new Rectangle(10, 480-6, 650-20 , 6);
+  Rectangle topWall = new Rectangle(WALL_X, TOP_WALL_Y, WALL_WIDTH, WALL_THICKNESS);
+  Rectangle bottomWall = new Rectangle(WALL_X, BOTTOM_WALL_Y, WALL_WIDTH, WALL_THICKNESS);
 
-	Rectangle rect1 = new Rectangle(30, player1, 17, 77);
-	Rectangle rect2 = new Rectangle(590, player2, 17, 77);
+  Rectangle playerOnePaddle = new Rectangle(LEFT_PADDLE_X, playerOneY, PADDLE_COLLISION_WIDTH, PADDLE_HEIGHT);
+  Rectangle playerTwoPaddle = new Rectangle(RIGHT_PADDLE_X, playerTwoY, PADDLE_COLLISION_WIDTH, PADDLE_HEIGHT);
+  Rectangle ballRect = new Rectangle(ballX, ballY, BALL_SIZE, BALL_SIZE);
 
-	Rectangle bola = new Rectangle(BallX, BallY, 15, 15);
-
-  Servindo(Socket clientSocket) {
+  ClientSession(Socket clientSocket) {
     this.clientSocket = clientSocket;
   }
 
   public void run() {
     try {
-      DataInputStream is = new DataInputStream(clientSocket.getInputStream());
-      os[cont++] = new DataOutputStream(clientSocket.getOutputStream());
+      DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+      clientOutputs[connectedClients++] = new DataOutputStream(clientSocket.getOutputStream());
 
-      new Out().start();
+      new StateWriter().start();
 
-	 if(cont==2){
-	  showTitleScreen=false;
-	  new Action_ball().start();
-	}
+      if (connectedClients == Servidor.MAX_PLAYERS) {
+        showTitleScreen = false;
+        new BallLoop().start();
+      }
 
-	char jogador=is.readChar();
+      char playerMarker = inputStream.readChar();
 
       do {
+        moveUp = inputStream.readBoolean();
+        moveDown = inputStream.readBoolean();
 
-				up = is.readBoolean();
-				down = is.readBoolean();
+        if (moveUp && !playerOnePaddle.intersects(topWall) && playerMarker == PLAYER_ONE_MARKER) {
+          playerOneY -= PADDLE_MOVE_STEP;
+        }
 
+        if (moveDown && !playerOnePaddle.intersects(bottomWall) && playerMarker == PLAYER_ONE_MARKER) {
+          playerOneY += PADDLE_MOVE_STEP;
+        }
 
-		    if(up==true && !rect1.intersects(parede_cima) && jogador=='1' )
-			player1 -=10;
+        if (moveUp && !playerTwoPaddle.intersects(topWall) && playerMarker == PLAYER_TWO_MARKER) {
+          playerTwoY -= PADDLE_MOVE_STEP;
+        }
 
-			if(down==true && !rect1.intersects(parede_baixo) && jogador=='1' )
-			player1 +=10;
+        if (moveDown && !playerTwoPaddle.intersects(bottomWall) && playerMarker == PLAYER_TWO_MARKER) {
+          playerTwoY += PADDLE_MOVE_STEP;
+        }
 
-			if(up==true && !rect2.intersects(parede_cima) && jogador=='2')
-			player2 -=10;
+        playerOnePaddle.setLocation(LEFT_PADDLE_X, playerOneY);
+        playerTwoPaddle.setLocation(RIGHT_PADDLE_X, playerTwoY);
 
-			if(down==true && !rect2.intersects(parede_baixo) && jogador=='2')
-			player2 +=10;
-
-
-			 rect1.setLocation(30,player1);
-			 rect2.setLocation(590,player2);
-
-
-		} while( true );
-
-
+      } while (true);
 
     } catch (IOException e) {
       e.printStackTrace();
     } catch (NoSuchElementException e) {
-      System.out.println("Conexacao terminada pelo cliente");
+      System.out.println("Connection closed by client");
     }
   }
 
-class Action_ball extends Thread{//classe interna que movimenta a bola
-private boolean verif=true;
+  class BallLoop extends Thread {
+    private boolean canBounceFromPlayerOne = true;
 
-	public void run(){
-		do {
-			bola.setLocation(BallX, BallY);
+    public void run() {
+      do {
+        ballRect.setLocation(ballX, ballY);
 
-		if(BallX<=0 || BallX>=650){
+        if (ballX <= 0 || ballX >= BOARD_WIDTH) {
+          if (ballX <= 0) {
+            playerTwoScore++;
+          }
 
-				if(BallX<=0)
-				   playerTwoScore++;
+          if (ballX >= BOARD_WIDTH) {
+            playerOneScore++;
+          }
 
-				if(BallX>=650)
-					playerOneScore++;
+          ballX = BALL_RESET_X;
+          mirroredBallX = OPPONENT_VIEW_BALL_RESET_X;
+          ballY = BALL_RESET_Y;
+        }
 
-			BallX=650/2;
-			Ball2X=600/2;
-			BallY=480/2;
-		 }
+        if (playerOnePaddle.intersects(ballRect) && canBounceFromPlayerOne) {
+          ballStepX = ballStepX * (-1);
+          canBounceFromPlayerOne = !canBounceFromPlayerOne;
+        }
 
+        if (playerTwoPaddle.intersects(ballRect) && !canBounceFromPlayerOne) {
+          ballStepX = ballStepX * (-1);
+          canBounceFromPlayerOne = !canBounceFromPlayerOne;
+        }
 
-		if(rect1.intersects(bola) && verif ){
-		      	fator1=fator1*(-1);
-            verif=!verif;
+        if (ballRect.intersects(topWall) || ballRect.intersects(bottomWall)) {
+          ballStepY = ballStepY * (-1);
+        }
+
+        mirroredBallX -= ballStepX * (-1);
+        ballX -= ballStepX;
+        ballY -= ballStepY;
+
+        try {
+          sleep(LOOP_SLEEP_MILLIS);
+        } catch (InterruptedException e) {
+        }
+
+      } while (true);
+    }
+  }
+
+  class StateWriter extends Thread {
+    int clientNumber;
+
+    public void run() {
+      clientNumber = connectedClients - 1;
+
+      try {
+        clientOutputs[clientNumber].writeInt(clientNumber);
+
+      } catch (IOException e) {
+        System.err.println("IOException:  " + e);
+      }
+
+      do {
+        try {
+          clientOutputs[clientNumber].writeBoolean(showTitleScreen);
+          clientOutputs[clientNumber].writeInt(playerOneScore);
+          clientOutputs[clientNumber].writeInt(playerTwoScore);
+
+          clientOutputs[clientNumber].writeInt(playerOneY);
+          clientOutputs[clientNumber].writeInt(playerTwoY);
+
+          if (clientNumber == 0) {
+            clientOutputs[clientNumber].writeInt(ballX);
+          }
+
+          if (clientNumber == 1) {
+            clientOutputs[clientNumber].writeInt(mirroredBallX);
+          }
+
+          clientOutputs[clientNumber].writeInt(ballY);
+          clientOutputs[clientNumber].flush();
+
+        } catch (IOException e) {
+          System.err.println("IOException:  " + e);
+        }
+
+        try {
+          sleep(LOOP_SLEEP_MILLIS);
+        } catch (InterruptedException e) {
+        }
+
+      } while (true);
+
     }
 
-    if(rect2.intersects(bola) && !verif){
-           fator1=fator1*(-1);
-           verif=!verif;
-    }
-
-
-		if(bola.intersects(parede_cima) || bola.intersects(parede_baixo) )
-			fator2=fator2*(-1);
-
-		Ball2X-=fator1*(-1);
-
-		BallX-=fator1;
-		BallY-=fator2;
-
-
-				try{
-				sleep(1000/60);
-				}catch(InterruptedException e){};
-
-		}while(true);
-	}
-
-}
-
-
-class Out extends Thread{//classe interna
-	int num_cliente;
-
-	public void run(){
-		    num_cliente=cont-1;
-
-		     try{
-				os[num_cliente].writeInt(num_cliente);
-
-		      } catch (IOException e) {System.err.println("IOException:  " + e);}
-
-			do{
-				try{
-						os[num_cliente].writeBoolean(showTitleScreen);
-						os[num_cliente].writeInt(playerOneScore);
-						os[num_cliente].writeInt(playerTwoScore);
-
-						os[num_cliente].writeInt(player1);
-						os[num_cliente].writeInt(player2);
-
-						if(num_cliente==0)
-							os[num_cliente].writeInt(BallX);
-
-						if(num_cliente==1)
-							os[num_cliente].writeInt(Ball2X);
-
-						os[num_cliente].writeInt(BallY);
-						os[num_cliente].flush();
-
-				} catch (IOException e) {System.err.println("IOException:  " + e);}
-
-				try{
-				sleep(1000/60);//cuidado com este sleep,se for muito pequeno sobrecarrega a rede enviando muitos dados
-				}catch(InterruptedException e){};
-
-
-			}while(true);
-
-
-
-	}
-
-}
-
+  }
 
 }
